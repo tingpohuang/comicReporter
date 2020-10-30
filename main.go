f8875cbd247427b534af021d04ae0b38cb80e66a
Tim Huang <timhuang@TW-LW60368-M.local>
2020-10-30 18:07:56 +0800

    Testing unmarshal

--- main.go
+++ main.go
@@ -54,7 +54,292 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					log.Println("Quota err:", err)
 				}
 				log.Println(message.Text)
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewImageMessage("https://mhfm1us.cdnmanhua.net/42/41957/20190701151032_180x240_19.jpg", "https://mhfm1us.cdnmanhua.net/42/41957/20190701151032_180x240_19.jpg")).Do(); err != nil {
+
+				jsonData := []byte(`{
+					"type": "carousel",
+					"contents": [
+					  {
+						"type": "bubble",
+						"body": {
+						  "type": "box",
+						  "layout": "vertical",
+						  "contents": [
+							{
+							  "type": "image",
+							  "url": "https://mhfm1us.cdnmanhua.net/42/41957/20190701151032_180x240_19.jpg",
+							  "size": "full",
+							  "aspectMode": "cover",
+							  "aspectRatio": "2:3",
+							  "gravity": "top"
+							},
+							{
+							  "type": "box",
+							  "layout": "vertical",
+							  "contents": [
+								{
+								  "type": "box",
+								  "layout": "vertical",
+								  "contents": [
+									{
+									  "type": "text",
+									  "text": "Brown's T-shirts",
+									  "size": "xl",
+									  "color": "#ffffff",
+									  "weight": "bold"
+									}
+								  ]
+								},
+								{
+								  "type": "box",
+								  "layout": "baseline",
+								  "contents": [
+									{
+									  "type": "text",
+									  "text": "¥35,800",
+									  "color": "#ebebeb",
+									  "size": "sm",
+									  "flex": 0
+									},
+									{
+									  "type": "text",
+									  "text": "¥75,000",
+									  "color": "#ffffffcc",
+									  "decoration": "line-through",
+									  "gravity": "bottom",
+									  "flex": 0,
+									  "size": "sm"
+									}
+								  ],
+								  "spacing": "lg"
+								},
+								{
+								  "type": "box",
+								  "layout": "vertical",
+								  "contents": [
+									{
+									  "type": "filler"
+									},
+									{
+									  "type": "box",
+									  "layout": "baseline",
+									  "contents": [
+										{
+										  "type": "filler"
+										},
+										{
+										  "type": "icon",
+										  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip14.png"
+										},
+										{
+										  "type": "text",
+										  "text": "Add to cart",
+										  "color": "#ffffff",
+										  "flex": 0,
+										  "offsetTop": "-2px",
+										  "action": {
+											"type": "uri",
+											"label": "action",
+											"uri": "http://linecorp.com/"
+										  },
+										  "contents": [
+											{
+											  "type": "span",
+											  "text": "hello, world"
+											}
+										  ]
+										},
+										{
+										  "type": "filler"
+										}
+									  ],
+									  "spacing": "sm"
+									},
+									{
+									  "type": "filler"
+									}
+								  ],
+								  "borderWidth": "1px",
+								  "cornerRadius": "4px",
+								  "spacing": "sm",
+								  "borderColor": "#ffffff",
+								  "margin": "xxl",
+								  "height": "40px"
+								}
+							  ],
+							  "position": "absolute",
+							  "offsetBottom": "0px",
+							  "offsetStart": "0px",
+							  "offsetEnd": "0px",
+							  "backgroundColor": "#03303Acc",
+							  "paddingAll": "20px",
+							  "paddingTop": "18px"
+							},
+							{
+							  "type": "box",
+							  "layout": "vertical",
+							  "contents": [
+								{
+								  "type": "text",
+								  "text": "SALE",
+								  "color": "#ffffff",
+								  "align": "center",
+								  "size": "xs",
+								  "offsetTop": "3px"
+								}
+							  ],
+							  "position": "absolute",
+							  "cornerRadius": "20px",
+							  "offsetTop": "18px",
+							  "backgroundColor": "#ff334b",
+							  "offsetStart": "18px",
+							  "height": "25px",
+							  "width": "53px"
+							}
+						  ],
+						  "paddingAll": "0px"
+						}
+					  },
+					  {
+						"type": "bubble",
+						"body": {
+						  "type": "box",
+						  "layout": "vertical",
+						  "contents": [
+							{
+							  "type": "image",
+							  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip2.jpg",
+							  "size": "full",
+							  "aspectMode": "cover",
+							  "aspectRatio": "2:3",
+							  "gravity": "top"
+							},
+							{
+							  "type": "box",
+							  "layout": "vertical",
+							  "contents": [
+								{
+								  "type": "box",
+								  "layout": "vertical",
+								  "contents": [
+									{
+									  "type": "text",
+									  "text": "Cony's T-shirts",
+									  "size": "xl",
+									  "color": "#ffffff",
+									  "weight": "bold"
+									}
+								  ]
+								},
+								{
+								  "type": "box",
+								  "layout": "baseline",
+								  "contents": [
+									{
+									  "type": "text",
+									  "text": "¥35,800",
+									  "color": "#ebebeb",
+									  "size": "sm",
+									  "flex": 0
+									},
+									{
+									  "type": "text",
+									  "text": "¥75,000",
+									  "color": "#ffffffcc",
+									  "decoration": "line-through",
+									  "gravity": "bottom",
+									  "flex": 0,
+									  "size": "sm"
+									}
+								  ],
+								  "spacing": "lg"
+								},
+								{
+								  "type": "box",
+								  "layout": "vertical",
+								  "contents": [
+									{
+									  "type": "filler"
+									},
+									{
+									  "type": "box",
+									  "layout": "baseline",
+									  "contents": [
+										{
+										  "type": "filler"
+										},
+										{
+										  "type": "icon",
+										  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip14.png"
+										},
+										{
+										  "type": "text",
+										  "text": "Add to cart",
+										  "color": "#ffffff",
+										  "flex": 0,
+										  "offsetTop": "-2px"
+										},
+										{
+										  "type": "filler"
+										}
+									  ],
+									  "spacing": "sm"
+									},
+									{
+									  "type": "filler"
+									}
+								  ],
+								  "borderWidth": "1px",
+								  "cornerRadius": "4px",
+								  "spacing": "sm",
+								  "borderColor": "#ffffff",
+								  "margin": "xxl",
+								  "height": "40px"
+								}
+							  ],
+							  "position": "absolute",
+							  "offsetBottom": "0px",
+							  "offsetStart": "0px",
+							  "offsetEnd": "0px",
+							  "backgroundColor": "#9C8E7Ecc",
+							  "paddingAll": "20px",
+							  "paddingTop": "18px"
+							},
+							{
+							  "type": "box",
+							  "layout": "vertical",
+							  "contents": [
+								{
+								  "type": "text",
+								  "text": "SALE",
+								  "color": "#ffffff",
+								  "align": "center",
+								  "size": "xs",
+								  "offsetTop": "3px"
+								}
+							  ],
+							  "position": "absolute",
+							  "cornerRadius": "20px",
+							  "offsetTop": "18px",
+							  "backgroundColor": "#ff334b",
+							  "offsetStart": "18px",
+							  "height": "25px",
+							  "width": "53px"
+							}
+						  ],
+						  "paddingAll": "0px"
+						}
+					  }
+					]
+				  }`)
+
+				container, err := linebot.UnmarshalFlexMessageJSON(jsonData)
+				// err is returned if invalid JSON is given that cannot be unmarshalled
+				if err != nil {
+					log.Print(err)
+				}
+				flexMessage := linebot.NewFlexMessage("alt text", container)
+
+				if _, err = bot.ReplyMessage(event.ReplyToken, flexMessage).Do(); err != nil {
 					log.Print(err)
 				}
 
