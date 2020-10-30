c169a6fe3318af46a9a584a7642bf0feddd56c6e
Tim Huang <timhuang@TW-LW60368-M.local>
2020-10-30 18:18:11 +0800

    test shoping content

--- main.go
+++ main.go
@@ -56,154 +56,181 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				log.Println(message.Text)
 
 				jsonData := []byte(`{
-					"type": "bubble",
-					"hero": {
-					  "type": "image",
-					  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-					  "size": "full",
-					  "aspectRatio": "20:13",
-					  "aspectMode": "cover",
-					  "action": {
-						"type": "uri",
-						"uri": "http://linecorp.com/"
-					  }
-					},
-					"body": {
-					  "type": "box",
-					  "layout": "vertical",
-					  "contents": [
-						{
-						  "type": "text",
-						  "text": "Brown Cafe",
-						  "weight": "bold",
-						  "size": "xl"
+					"type": "carousel",
+					"contents": [
+					  {
+						"type": "bubble",
+						"hero": {
+						  "type": "image",
+						  "size": "full",
+						  "aspectRatio": "20:13",
+						  "aspectMode": "cover",
+						  "url": "https://mhfm1us.cdnmanhua.net/42/41957/20190701151032_180x240_19.jpg"
 						},
-						{
+						"body": {
 						  "type": "box",
-						  "layout": "baseline",
-						  "margin": "md",
+						  "layout": "vertical",
+						  "spacing": "sm",
 						  "contents": [
 							{
-							  "type": "icon",
-							  "size": "sm",
-							  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+							  "type": "text",
+							  "text": "Arm Chair, White",
+							  "wrap": true,
+							  "weight": "bold",
+							  "size": "xl"
 							},
 							{
-							  "type": "icon",
-							  "size": "sm",
-							  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+							  "type": "box",
+							  "layout": "baseline",
+							  "contents": [
+								{
+								  "type": "text",
+								  "text": "$49",
+								  "wrap": true,
+								  "weight": "bold",
+								  "size": "xl",
+								  "flex": 0
+								},
+								{
+								  "type": "text",
+								  "text": ".99",
+								  "wrap": true,
+								  "weight": "bold",
+								  "size": "sm",
+								  "flex": 0
+								}
+							  ]
+							}
+						  ]
+						},
+						"footer": {
+						  "type": "box",
+						  "layout": "vertical",
+						  "spacing": "sm",
+						  "contents": [
+							{
+							  "type": "button",
+							  "style": "primary",
+							  "action": {
+								"type": "uri",
+								"label": "Add to Cart",
+								"uri": "https://linecorp.com"
+							  }
 							},
 							{
-							  "type": "icon",
-							  "size": "sm",
-							  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+							  "type": "button",
+							  "action": {
+								"type": "uri",
+								"label": "Add to wishlist",
+								"uri": "https://linecorp.com"
+							  }
+							}
+						  ]
+						}
+					  },
+					  {
+						"type": "bubble",
+						"hero": {
+						  "type": "image",
+						  "size": "full",
+						  "aspectRatio": "20:13",
+						  "aspectMode": "cover",
+						  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png"
+						},
+						"body": {
+						  "type": "box",
+						  "layout": "vertical",
+						  "spacing": "sm",
+						  "contents": [
+							{
+							  "type": "text",
+							  "text": "Metal Desk Lamp",
+							  "wrap": true,
+							  "weight": "bold",
+							  "size": "xl"
 							},
 							{
-							  "type": "icon",
-							  "size": "sm",
-							  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-							},
-							{
-							  "type": "icon",
-							  "size": "sm",
-							  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+							  "type": "box",
+							  "layout": "baseline",
+							  "flex": 1,
+							  "contents": [
+								{
+								  "type": "text",
+								  "text": "$11",
+								  "wrap": true,
+								  "weight": "bold",
+								  "size": "xl",
+								  "flex": 0
+								},
+								{
+								  "type": "text",
+								  "text": ".99",
+								  "wrap": true,
+								  "weight": "bold",
+								  "size": "sm",
+								  "flex": 0
+								}
+							  ]
 							},
 							{
 							  "type": "text",
-							  "text": "4.0",
-							  "size": "sm",
-							  "color": "#999999",
+							  "text": "Temporarily out of stock",
+							  "wrap": true,
+							  "size": "xxs",
 							  "margin": "md",
+							  "color": "#ff5551",
 							  "flex": 0
 							}
 						  ]
 						},
-						{
+						"footer": {
 						  "type": "box",
 						  "layout": "vertical",
-						  "margin": "lg",
 						  "spacing": "sm",
 						  "contents": [
 							{
-							  "type": "box",
-							  "layout": "baseline",
-							  "spacing": "sm",
-							  "contents": [
-								{
-								  "type": "text",
-								  "text": "Place",
-								  "color": "#aaaaaa",
-								  "size": "sm",
-								  "flex": 1
-								},
-								{
-								  "type": "text",
-								  "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-								  "wrap": true,
-								  "color": "#666666",
-								  "size": "sm",
-								  "flex": 5
-								}
-							  ]
+							  "type": "button",
+							  "flex": 2,
+							  "style": "primary",
+							  "color": "#aaaaaa",
+							  "action": {
+								"type": "uri",
+								"label": "Add to Cart",
+								"uri": "https://linecorp.com"
+							  }
 							},
 							{
-							  "type": "box",
-							  "layout": "baseline",
-							  "spacing": "sm",
-							  "contents": [
-								{
-								  "type": "text",
-								  "text": "Time",
-								  "color": "#aaaaaa",
-								  "size": "sm",
-								  "flex": 1
-								},
-								{
-								  "type": "text",
-								  "text": "10:00 - 23:00",
-								  "wrap": true,
-								  "color": "#666666",
-								  "size": "sm",
-								  "flex": 5
-								}
-							  ]
+							  "type": "button",
+							  "action": {
+								"type": "uri",
+								"label": "Add to wish list",
+								"uri": "https://linecorp.com"
+							  }
 							}
 						  ]
 						}
-					  ]
-					},
-					"footer": {
-					  "type": "box",
-					  "layout": "vertical",
-					  "spacing": "sm",
-					  "contents": [
-						{
-						  "type": "button",
-						  "style": "link",
-						  "height": "sm",
-						  "action": {
-							"type": "uri",
-							"label": "CALL",
-							"uri": "https://linecorp.com"
-						  }
-						},
-						{
-						  "type": "button",
-						  "style": "link",
-						  "height": "sm",
-						  "action": {
-							"type": "uri",
-							"label": "WEBSITE",
-							"uri": "https://linecorp.com"
-						  }
-						},
-						{
-						  "type": "spacer",
-						  "size": "sm"
+					  },
+					  {
+						"type": "bubble",
+						"body": {
+						  "type": "box",
+						  "layout": "vertical",
+						  "spacing": "sm",
+						  "contents": [
+							{
+							  "type": "button",
+							  "flex": 1,
+							  "gravity": "center",
+							  "action": {
+								"type": "uri",
+								"label": "See more",
+								"uri": "https://linecorp.com"
+							  }
+							}
+						  ]
 						}
-					  ],
-					  "flex": 0
-					}
+					  }
+					]
 				  }`)
 
 				container, err := linebot.UnmarshalFlexMessageJSON(jsonData)
